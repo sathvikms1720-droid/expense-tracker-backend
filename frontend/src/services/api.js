2c170e7714fb751abee9dd06488bdfc73b00e2b4
sathvikms1720-droid <sathvikms1720@gmail.com>
2026-07-02 21:42:59 +0530

    Updated API URL to Render production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://expense-tracker-backend-1-ctwy.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
